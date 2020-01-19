f4b61c146439e5cc613338d0b8bb996e4e6e4156
YashSaxena9 <yashsaxena962000@gmail.com>
2020-01-19 23:16:31 +0530

    graphs modified

--- lecture_041_graph/cppFiles/graph.cpp
+++ lecture_041_graph/cppFiles/graph.cpp
@@ -17,6 +17,11 @@ public:
         this->v = v;
         this->w = w;
     }
+    
+    Edge(int v) {
+        this->v = v;
+        this->w = 0;
+    }
 
     Edge() {
         //  default
@@ -38,6 +43,20 @@ void display() {
     cout << endl;
 }
 
+void display(vector<vector<Edge*>> &graph_) {
+    for (int i = 0; i < graph_.size(); i++) {
+        cout << i << " -> ";
+        for (Edge* e : graph_[i]) {
+            cout << "(" << e->v << ", " << e->w<< ") ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
+//  ---------- add edges ------------
+
+//  for undirected weighted
 void addEdge(int u, int v, int w) {
     if (u < 0 || u >= graph.size() || v < 0 || v >= graph.size()) {
         return;
@@ -45,6 +64,45 @@ void addEdge(int u, int v, int w) {
     graph[u].push_back(new Edge(v, w));
     graph[v].push_back(new Edge(u, w));
 }
+
+//  for undirected non-weighted
+void addEdge(int u, int v) {
+    if (u < 0 || u >= graph.size() || v < 0 || v >= graph.size()) {
+        return;
+    }
+    graph[u].push_back(new Edge(v, 0));
+    graph[v].push_back(new Edge(u, 0));
+}
+
+//  for unidirectional weighted
+void addEdge_uni(int u, int v, int w) {
+    if (u < 0 || u >= graph.size() || v < 0 || v >= graph.size()) {
+        return;
+    }
+    graph[u].push_back(new Edge(v, w));
+}
+
+//  for unidirectional non-weighted
+void addEdge_uni(int u, int v) {
+    if (u < 0 || u >= graph.size() || v < 0 || v >= graph.size()) {
+        return;
+    }
+    graph[u].push_back(new Edge(v, 0));
+}
+
+//  for mixed weighted
+void addEdge(int u, int v, int w, bool isDir_) {
+    if (u < 0 || u >= graph.size() || v < 0 || v >= graph.size()) {
+        return;
+    }
+    graph[u].push_back(new Edge(v, w));
+    if (!isDir_) {
+        graph[v].push_back(new Edge(u, w));
+    }
+}
+
+
+//  ---------- end ------------
 
 void removeEdge(int u, int v) {
     //  method 1
@@ -276,23 +334,268 @@ int hamiltonianPath(int start, int head, int size, vector<bool> &vis, string ans
     return count;
 }
 
+//  class for coloring of vertexes
+class ColorPair {
+    public:
+        int vtx;
+        int color;
+        ColorPair(int vtx, int color) {
+            this->color = color;
+            this->vtx = vtx;
+        }
+        ColorPair() {
+            //  default
+        }
+};
+
+bool isBipartite_util_01(int src, vector<int> &vis) {
+    queue<ColorPair*> que;
+    ColorPair* cp = new ColorPair(src, 0);
+    que.push(cp);
+    bool flag = true;
+    while (que.size() != 0) {
+        ColorPair* front = que.front();
+        que.pop();
+        if (vis[front->vtx] != -1) {
+            if (front->color != vis[front->vtx]) {
+                cout << "conflict : " << endl;
+                flag = false;
+            }
+            continue;
+        }
+        vis[front->vtx] = front->color;
+        for (Edge* e : graph[front->vtx]) {
+            if (vis[e->v] == -1) {
+                que.push(new ColorPair(e->v, (front->color + 1) % 2));
+            }
+        }
+    }
+    return flag;
+}
+
+bool isBipartite_util_02(int src, vector<int> &vis) {
+    queue<pair<int, int>> que;
+    que.push({src, 0});
+    bool flag = true;
+    while (que.size() != 0) {
+        pair<int, int> front = que.front();
+        que.pop();
+        if (vis[front.first] != -1) {
+            if (front.second != vis[front.first]) {
+                cout << "conflict : " << endl;
+                flag = false;
+            }
+            continue;
+        }
+        vis[front.first] = front.second;
+        for (Edge* e : graph[front.first]) {
+            if (vis[e->v] == -1) {
+                que.push({e->v, (front.second + 1) % 2});
+            }
+        }
+    }
+    return flag;
+}
+
+bool isBipartite() {
+    vector<int> vis(graph.size(), -1);
+    int count = 0;
+    bool flag = false;
+    for (int i = 0; i < graph.size(); i++) {
+        if (vis[i] == -1) {
+            bool ans = isBipartite_util_01(i, vis);
+            flag = flag || ans;
+            cout << count << " " << (boolalpha) << ans << endl;
+            count++;
+        }
+    }
+    return flag;
+}
+
+//  --------------- topological sort by DFS ------------------
+//  topological sort cycle not handled
+void topologicalSort_util_(int src, vector<bool> &vis, vector<int> &stack) {
+    vis[src] = true;
+    for (Edge* e : graph[src]) {
+        if (!vis[e->v]) {
+            topologicalSort_util_(e->v, vis, stack);
+        }
+    }
+    stack.push_back(src);
+}
+
+void topologicalSort_() {
+    vector<bool> vis(graph.size(), false);
+    vector<int> stack;
+    for (int i = 0; i < graph.size(); i++) {
+        if (!vis[i]) {
+            topologicalSort_util_(i, vis, stack);
+        }
+    }
+    while (stack.size() != 0) {
+        cout << stack.back() << " ";
+        stack.pop_back();
+    }
+    cout << endl;
+}
+
+//  topological sort cycle handled
+bool topologicalSort_util_btr(int src, vector<bool> &vis, vector<bool> &cycleDetector, vector<int> &stack) {
+    vis[src] = true;
+    cycleDetector[src] = true;
+    bool res = false;
+    for (Edge* e : graph[src]) {
+        if (!vis[e->v]) {
+            res = res || topologicalSort_util_btr(e->v, vis, cycleDetector, stack);
+        } else if (cycleDetector[e->v]) {
+            return true;
+        }
+    }
+    cycleDetector[src] = false;
+    stack.push_back(src);
+    return res;
+}
+
+void topologicalSort_btr() {
+    vector<bool> vis(graph.size(), false);
+    vector<bool> cycleDetector(graph.size(), false);
+    vector<int> stack;
+    bool isCycle = false;
+    for (int i = 0; i < graph.size(); i++) {
+        if (!vis[i]) {
+            isCycle = isCycle || topologicalSort_util_btr(i, vis, cycleDetector, stack);
+        }
+    }
+    if (isCycle) {
+        cout << "invalid cycle present" << endl;
+        return;
+    }
+    while (stack.size() != 0) {
+        cout << stack.back() << " ";
+        stack.pop_back();
+    }
+    cout << endl;
+}
+
+//  --------------- topological sort by BFS ------------------
+//  Khan's Algo
+void topologicalSort_bfs() {
+    vector<int> inDegree(graph.size(), 0);
+    for (int i = 0; i < inDegree.size(); i++) {
+        for (Edge* e : graph[i]) {
+            inDegree[e->v]++;
+        }
+    }
+    queue<int> que;
+    for (int i = 0; i < inDegree.size(); i++) {
+        // cout<<inDegree[i]<<" ";
+        if (inDegree[i] == 0) {
+            // cout<<"i"<< i<<endl;
+            que.push(i);
+        }
+    }
+    vector<int> ans;
+    while (que.size() != 0) {
+        int rvtx = que.front();
+        que.pop();
+        ans.push_back(rvtx);
+        for (Edge* e : graph[rvtx]) {
+            inDegree[e->v]--;
+            if (inDegree[e->v] == 0) {
+                que.push(e->v);
+            }
+        }
+    }
+
+    //  checking ans covers all vertex or not
+    if (ans.size() != graph.size()) {
+        cout << "invalid cycle present" << endl;
+    } else {
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+    }
+    cout << endl;
+}
+
+//  Kosa Raju Algo  for strongly connected components
+vector<vector<Edge*>> inverseGraph() {
+    vector<vector<Edge*>> invGraph(graph.size(), vector<Edge*>());
+    for (int i = 0; i < graph.size(); i++) {
+        for (Edge* e : graph[i]) {
+            invGraph[e->v].push_back(new Edge(i));
+        }
+    }
+    // display(invGraph);
+    return invGraph;
+}
+
+/* ================================================ */
 //  ----------- main -------------
 void constructGraph() {
-    int tot_vert = 7;
+    int tot_vert = 10;
     for (int i = 0; i < tot_vert; i++) {
         graph.push_back(vector<Edge*>());
     }
-    addEdge(0, 1, 10);
-    addEdge(0, 3, 10);
-    addEdge(1, 2, 10);
-    addEdge(2, 3, 40);
-    addEdge(3, 4, 2);
-    addEdge(4, 5, 2);
-    addEdge(4, 6, 3);
-    addEdge(5, 6, 8);
-    //  extra
-    addEdge(6, 0, 100);
-    addEdge(2, 5, 100);
+
+    //  graph 1
+    // addEdge(0, 1, 10);
+    // addEdge(0, 3, 10);
+    // addEdge(1, 2, 10);
+    // addEdge(2, 3, 40);
+    // addEdge(3, 4, 2);
+    // addEdge(4, 5, 2);
+    // addEdge(4, 6, 3);
+    // addEdge(5, 6, 8);
+           // extra
+    // addEdge(6, 0, 100);
+    // addEdge(2, 5, 100);
+
+    //  graph 2
+    // addEdge(1, 2, 6);
+    // addEdge(1, 3, 6);
+    // addEdge(0, 2, 6);
+    // addEdge(0, 3, 6);
+    
+    //  graph 3                 /* uni directional */
+    // addEdge_uni(0, 1);
+    // addEdge_uni(1, 2);
+    // addEdge_uni(2, 3);
+    // addEdge_uni(0, 4);
+    // addEdge_uni(7, 4);
+    // addEdge_uni(7, 6);
+    // addEdge_uni(6, 5);
+    // addEdge_uni(5, 3);
+
+    // graph 4
+    // addEdge_uni(0, 1);
+    // addEdge_uni(1, 2);
+    // addEdge_uni(2, 3);
+    // addEdge_uni(2, 4);
+    // addEdge_uni(4, 6);
+    // addEdge_uni(6, 7);
+    // addEdge_uni(7, 4);
+    // addEdge_uni(1, 3);
+    // addEdge_uni(3, 4);
+    // addEdge_uni(3, 5);
+
+    // graph 5
+    addEdge_uni(0, 1);
+    addEdge_uni(1, 3);
+    addEdge_uni(3, 8);
+    addEdge_uni(8, 9);
+    addEdge_uni(9, 8);
+    addEdge_uni(2, 0);
+    addEdge_uni(1, 2);
+    addEdge_uni(2, 4);
+    addEdge_uni(4, 3);
+    addEdge_uni(2, 5);
+    addEdge_uni(6, 4);
+    addEdge_uni(5, 6);
+    addEdge_uni(5, 7);
+    addEdge_uni(3, 7);
+    addEdge_uni(7, 6);
+    addEdge_uni(7, 9);
 }
 
 int main(int args, char**argv) {
@@ -318,7 +621,16 @@ int main(int args, char**argv) {
     // bfs1(0, 6);
     // bfs1_corrected(0, 6);
     // bfs2(0, 6);
-    vector<bool> vis4(graph.size(), false);
-    cout << hamiltonianPath(0, 0, 0, vis4, "");
+
+    // vector<bool> vis4(graph.size(), false);
+    // cout << hamiltonianPath(0, 0, 0, vis4, "");
+
+    // cout << isBipartite() << endl;
+
+    // topologicalSort_();
+    // topologicalSort_btr();
+    // topologicalSort_bfs();
+    inverseGraph();
+    
     return 0;
 }
