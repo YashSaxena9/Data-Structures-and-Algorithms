0a98f23f198f960994de0d0b6633448299c12c0a
YashSaxena9 <yashsaxena962000@gmail.com>
2020-03-26 01:29:01 +0530

    generic hashmap

--- lecture_048_generics/myHashMap/Client.java
+++ lecture_048_generics/myHashMap/Client.java
@@ -7,6 +7,7 @@ public class Client {
 
     public static void main(String[] args) {
         MyHashMap map = new MyHashMap();
+        MyHashMap_generic<String, Boolean> map2 = new MyHashMap_generic<>();
         map.put(999, 1000);
         map.put(998, 40);
         map.put(989, 99);
@@ -20,5 +21,9 @@ public class Client {
         for (int k : map.keyArray()) {
             System.out.print(map.get(k) + " " + k + ", ");
         } System.out.println();
+        map2.put("yash", true);
+        map2.put("abc", false);
+        System.out.println(map2);
+        System.out.println(map2.get("abc"));
     }
 }
